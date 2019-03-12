d315e1c4bbab4c73f1e7bc44f6eebe7f8060dfb0
geraosio <geraosio@gmail.com>
2019-03-12 05:48:19 -0600

    Add a new Xcode project

---
+++ Sailing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sailing
+//
+//  Created by Gerardo Osio on 3/12/19.
+//  Copyright © 2019 Gerardo Osio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
